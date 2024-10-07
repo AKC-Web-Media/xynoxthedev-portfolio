d0b5414ac80249e86976602e47273502f8bdd0d9
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-10-08 01:54:13 +0530

    ADD: Nav Routes

---
+++ app/projects/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar/page"
+
+export default function Projects(){
+    return <>
+        <Navbar />
+        <div className="flex mt-32 items-center justify-center">
+            <p className="text-center text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+                Projects
+            </p>
+        </div>
+    </>
+}
