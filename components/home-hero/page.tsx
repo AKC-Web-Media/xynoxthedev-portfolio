bb4cb1023970f035f4d4d4ada7d01f30d3b9baaf
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-10-27 01:44:18 +0530

    ADD: Added all the link buttons

--- components/home-hero/page.tsx
+++ components/home-hero/page.tsx
@@ -4,6 +4,12 @@ import Link from "next/link";
 import github from "@/public/svg/github.svg";
 import x from "@/public/svg/x.svg";
 import linkedin from "@/public/svg/linkedin.svg";
+import email from "@/public/svg/email.svg";
+import instagram from "@/public/svg/instagram.svg";
+import anilist from "@/public/svg/anilist.svg";
+import steam from "@/public/svg/steam.svg";
+import spotify from "@/public/svg/spotify.svg";
+import buymeacoffee from "@/public/svg/buymeacoffee.svg";
 
 export default function HomeHero() {
   return (
@@ -29,54 +35,154 @@ export default function HomeHero() {
                 </p>
 
                 <hr className="" />
-                <div className="flex justify-between my-3 p-6">
+                <div className="flex flex-col justify-between my-3 p-6">
+                    <div className="flex justify-between my-2">
+                        <Link
+                            href={""}
+                            className="flex items-center gap-2"
+                            target="_blank"
+                            >
+                            <Image 
+                                src={linkedin}
+                                alt="LinkedIn"
+                                width={25}
+                                height={25}
+                                />
+                            <p className="font-bold">
+                                LinkedIn
+                            </p>
+                        </Link>
+                        <Link
+                            href={"https://github.com/xynoxthedev"}
+                            className="flex items-center gap-2"
+                            target="_blank"
+                            >
+                            <Image 
+                                src={github}
+                                alt="GitHub"
+                                width={25}
+                                height={25}
+                                />
+                            <p className="font-bold">
+                                GitHub
+                            </p>
+                        </Link>
+                        <Link
+                            href={""}
+                            className="flex items-center gap-2"
+                            target="_blank"
+                            >
+                            <Image 
+                                src={email}
+                                alt="X"
+                                width={25}
+                                height={25}
+                                />
+                            <p className="font-bold">
+                                Email
+                            </p>
+                        </Link>
+                    </div>
+                    <div className="flex justify-between my-2">
                     <Link
-                        href={""}
-                        className="flex items-center space-x-2"
-                    >
-                        <Image 
-                            src={github}
-                            alt="GitHub"
-                            width={25}
-                            height={25}
-                        />
-                        <p className="font-bold">
-                            GitHub
-                        </p>
-                    </Link>
+                            href={""}
+                            className="flex items-center gap-2"
+                            target="_blank"
+                            >
+                            <Image 
+                                src={instagram}
+                                alt="LinkedIn"
+                                width={25}
+                                height={25}
+                                />
+                            <p className="font-bold">
+                                Instagram
+                            </p>
+                        </Link>
+                        <Link
+                            href={"https://github.com/xynoxthedev"}
+                            className="flex items-center gap-2"
+                            target="_blank"
+                            >
+                            <Image 
+                                src={buymeacoffee}
+                                alt="GitHub"
+                                width={25}
+                                height={25}
+                                />
+                            <p className="font-bold">
+                                buy me a coffee
+                            </p>
+                        </Link>
+                        <Link
+                            href={""}
+                            className="flex items-center gap-2"
+                            target="_blank"
+                            >
+                            <Image 
+                                src={x}
+                                alt="X"
+                                width={25}
+                                height={25}
+                                />
+                            <p className="font-bold">
+                                X
+                            </p>
+                        </Link>
+                    </div>
+                    <div className="flex justify-between my-2">
                     <Link
-                        href={""}
-                        className="flex items-center space-x-2"
-                    >
-                        <Image 
-                            src={x}
-                            alt="X"
-                            width={25}
-                            height={25}
-                        />
-                        <p className="font-bold">
-                            Twitter
-                        </p>
-                    </Link>
-                    <Link
-                        href={""}
-                        className="flex items-center space-x-2"
-                    >
-                        <Image 
-                            src={linkedin}
-                            alt="LinkedIn"
-                            width={25}
-                            height={25}
-                        />
-                        <p className="font-bold">
-                            LinkedIn
-                        </p>
-                    </Link>
+                            href={""}
+                            className="flex items-center gap-2"
+                            target="_blank"
+                            >
+                            <Image 
+                                src={anilist}
+                                alt="LinkedIn"
+                                width={25}
+                                height={25}
+                                className="invert"
+                                />
+                            <p className="font-bold">
+                                Anilist
+                            </p>
+                        </Link>
+                        <Link
+                            href={""}
+                            className="flex items-center gap-2"
+                            target="_blank"
+                        >
+                            <Image 
+                                src={steam}
+                                alt="GitHub"
+                                width={25}
+                                height={25}
+                            />
+                            <p className="font-bold">
+                                Steam
+                            </p>
+                        </Link>
+                        <Link
+                            href={""}
+                            className="flex items-center gap-2"
+                            target="_blank"
+                        >
+                            <Image 
+                                src={spotify}
+                                alt="X"
+                                width={25}
+                                height={25}
+                            />
+                            <p className="font-bold">
+                                Spotify
+                            </p>
+                        </Link>
+                    </div>
                 </div>
                 
             </div>
             <div className="w-full justify-center hidden md:flex">
-                <Image src={profile} alt="Arindam Hazra" width={500} />
+                <Image src={profile} alt="Arindam Hazra" width={450} />
             </div>
         </div>
     </>
