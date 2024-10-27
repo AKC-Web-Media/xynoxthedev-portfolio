99f058451762d024b561a417d5f60a73715937d6
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-10-27 20:37:40 +0530

    ADD: Added more meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,49 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Arindam Hazra | XynoxTheDev",
-  description: "Made by AdityaKrCodes",
+    title: "Arindam Hazra | XynoxTheDev",
+    description: "Full Stack Developer | Cloud Enthusiast | Open Source Contributor",
+    robots: {
+    index: true,
+    follow: true,
+    },
+    authors: [
+        { name: 'Arindam Hazra', url: 'https://github.com/XynoxTheDev' },
+    ],
+    metadataBase: new URL('https://arindamhazra.xyz/'),
+    keywords: [
+        'Arindam Hazra',
+        'XynoxTheDev',
+        'Full Stack Developet',
+        'Cloud Enthusiast',
+        'Open Source Contributor',
+        'Portfolio',
+        'software',
+        'development',
+        'software-development',
+        'website',
+        'website-development',
+        'web-development',
+        'web',
+        'landing',
+        'page',
+        'discord',
+        'bot',
+        'app',
+        'mobile',
+        'consulting',
+        'it',
+        'information',
+        'technology',
+        'internet',
+        'discordbot',
+        'professional',
+        'business',
+        'startup',
+        'portfolio',
+    ],
 };
+    
 
 export default function RootLayout({
   children,
