3e166154ed69bf45a3346308eb33e841df85059c
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-10-27 19:55:03 +0530

    Made the design consistent

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar/page"
 export default function Blogs(){
     return <>
         <Navbar />
-        <div className="flex flex-col mt-32 items-center justify-center w-10/12 mx-auto">
+        <div className="flex flex-col mt-10 items-center justify-center w-10/12 mx-auto">
             <h1 className="text-center text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
                 Blogs
             </h1>
